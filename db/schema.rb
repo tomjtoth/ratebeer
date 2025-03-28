6b03e886de4a27af8fbd08890d25856751781cea
Tamás Tóth <tomjtoth@gmail.com>
2025-03-28 13:12:06 +0200

    rails g scaffold Beer name:string style:string brewery_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_28_054344) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_28_105334) do
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.string "style"
+    t.integer "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.integer "year"
