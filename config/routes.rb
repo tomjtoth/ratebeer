2840327c53cf219426255aa35d84f8a1f3eba245
Tamás Tóth <tomjtoth@gmail.com>
2025-03-29 18:25:59 +0200

    create/destroy session via `/signin` and /`/signout`

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,6 @@ Rails.application.routes.draw do
   resources :ratings, only: [ :index, :new, :create, :destroy ]
 
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 end
