9a113eeb78e514856284e4c89117b64529d8ed19
Tamás Tóth <tomjtoth@gmail.com>
2025-03-29 21:43:06 +0200

    show ratings of each user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @ratings = Rating.where(user_id: @user.id)
   end
 
   # GET /users/new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :ratings
+  include RatingAverage
 end
