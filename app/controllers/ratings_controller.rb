2f10d3fc9c009b99e5938a00678a258063942faf
Tamás Tóth <tomjtoth@gmail.com>
2025-03-29 18:13:06 +0200

    store last rating in session

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,7 +9,12 @@ class RatingsController < ApplicationController
   end
 
   def create
-    Rating.create params.require(:rating).permit(:score, :beer_id)
+    # otetaan luotu reittaus muuttujaan
+    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
+
+    # talletetaan tehty reittaus sessioon
+    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
+
     redirect_to ratings_path
   end
 
