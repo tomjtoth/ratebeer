86e8a3e1538066dc32ae9292c1d72b2f4fb9cc4f
Tamás Tóth <tomjtoth@gmail.com>
2025-03-29 11:41:12 +0200

    def Brewery.restart

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -6,4 +6,9 @@ class Brewery < ApplicationRecord
         puts "established at year #{year}"
         puts "number of beers #{beers.count}"
     end
+
+    def restart
+        self.year = 2022
+        puts "changed year to #{year}"
+    end
 end
