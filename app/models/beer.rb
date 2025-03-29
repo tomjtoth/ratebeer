a5f2f761c6e5b8d556bdb261221f64e2410b7886
Tamás Tóth <tomjtoth@gmail.com>
2025-03-29 12:59:15 +0200

    impl Beer.average_rating

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        sum=0
+        ratings.each do |r|
+            sum += r.score
+        end
+        (sum / ratings.size).to_f
+    end
 end
