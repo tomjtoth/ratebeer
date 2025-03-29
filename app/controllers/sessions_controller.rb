507ff5eca0ce428beb42d0fe33cd2e27a6e8ff5e
Tamás Tóth <tomjtoth@gmail.com>
2025-03-29 18:15:41 +0200

    impl SessionController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+    # renderöi kirjautumissivun
+  end
+
+  def create
+    user = User.find_by username: params[:username]
+    session[:user_id] = user.id if user
+    redirect_to user
+  end
+
+  def destroy
+    # nollataan sessio
+    session[:user_id] = nil
+    # uudelleenohjataan sovellus pääsivulle
+    redirect_to :root
+  end
+end
