60be056650022a0d1aa74880c855c9ea58c3a236
Tamás Tóth <tomjtoth@gmail.com>
2025-03-29 13:37:27 +0200

    route req `POST /ratings`

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
   get "kaikki_bisset", to: "beers#index"
   get "ratings", to: "ratings#index"
   get "ratings/new", to: "ratings#new"
+  post "ratings", to: "ratings#create"
 end
