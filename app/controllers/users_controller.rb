8168cc08f691d216a28b2b4fba65ebee9818f88c
Tamás Tóth <tomjtoth@gmail.com>
2025-03-30 13:00:10 +0300

    user password is asked twice upon registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.expect(user: [ :username ])
+      params.require(:user).permit(:username, :password, :password_confirmation)
     end
 end
