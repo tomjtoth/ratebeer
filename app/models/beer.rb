64983c207e84ad00275dc1f4ae7ccffaba0c5bb7
Tamás Tóth <tomjtoth@gmail.com>
2025-03-30 15:54:27 +0300

    beer needs a valid brewery_id to exist upon creation

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,6 +1,7 @@
 class Beer < ApplicationRecord
     validates :name, length: { minimum: 1 }
     validates :style, length: { minimum: 1 }
+    validate :assigned_brewery_exists
 
     belongs_to :brewery
     has_many :ratings, dependent: :destroy
@@ -16,4 +17,13 @@ class Beer < ApplicationRecord
 
       ratings.map { |r| r.score }.sum / ratings.count.to_f
     end
+
+    private 
+      def assigned_brewery_exists
+        errors.add(:brewery, "does not exist") if not (
+          brewery and 
+          brewery.id and 
+          Brewery.find(brewery.id)
+        )
+      end
 end
