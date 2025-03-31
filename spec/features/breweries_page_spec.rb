fb1656cad40014db8f1b16bf017c40a823000caa
Tamás Tóth <tomjtoth@gmail.com>
2025-03-31 13:59:56 +0300

    1st tests via browser

---
+++ spec/features/breweries_page_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Breweries page" do
+  it "should not have any before been created" do
+    visit_n_print breweries_path
+    expect(page).to have_content 'Breweries'
+    expect(page).to have_content 'Number of breweries: 0'
+  end
+end
+
+def visit_n_print(path)
+    visit path
+    puts page.html
+end
