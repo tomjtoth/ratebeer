63a905920b821766e4c80282406a5fe75fb63c54
Tamás Tóth <tomjtoth@gmail.com>
2025-03-31 12:59:26 +0300

    fixed average_rating (dividing float with integer)

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -3,6 +3,6 @@ module RatingAverage
 
   def average_rating
     total_score = ratings.reduce(0) { |sum, r| sum + r.score }
-    (total_score / ratings.size).to_f
+    total_score.to_f / ratings.size
   end
 end
