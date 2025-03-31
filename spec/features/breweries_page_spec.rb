28d18f25049b752b382e7735661a723620b1d8fd
Tamás Tóth <tomjtoth@gmail.com>
2025-03-31 14:12:03 +0300

    added test for existing breweries

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -6,6 +6,21 @@ describe "Breweries page" do
         expect(page).to have_content 'Breweries'
         expect(page).to have_content 'Number of breweries: 0'
     end
+
+    it "lists the existing breweries and their total number" do
+        breweries = ["Koff", "Karjala", "Schlenkerla"]
+        breweries.each do |brewery_name|
+            FactoryBot.create(:brewery, name: brewery_name)
+        end
+
+        visit_n_print breweries_path
+
+        expect(page).to have_content "Number of breweries: #{breweries.count}"
+
+        breweries.each do |brewery_name|
+            expect(page).to have_content brewery_name
+        end
+    end
 end
 
 def visit_n_print(path)
