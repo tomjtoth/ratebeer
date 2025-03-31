6c39fcca6b5990e5591a7bde1c7a6fbf7f33ebe4
Tamás Tóth <tomjtoth@gmail.com>
2025-03-31 14:16:29 +0300

    refactored tests involving browser

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -7,34 +7,31 @@ describe "Breweries page" do
         expect(page).to have_content 'Number of breweries: 0'
     end
 
-    it "lists the existing breweries and their total number" do
-        breweries = ["Koff", "Karjala", "Schlenkerla"]
-        breweries.each do |brewery_name|
-            FactoryBot.create(:brewery, name: brewery_name)
+    describe "when breweries exists" do
+        before :each do
+            # jotta muuttuja näkyisi it-lohkoissa, tulee sen nimen alkaa @-merkillä
+            @breweries = ["Koff", "Karjala", "Schlenkerla"]
+            year = 1896
+            @breweries.each do |brewery_name|
+                FactoryBot.create(:brewery, name: brewery_name, year: year += 1)
+            end
+        
+            visit breweries_path
         end
-
-        visit_n_print breweries_path
-
-        expect(page).to have_content "Number of breweries: #{breweries.count}"
-
-        breweries.each do |brewery_name|
-            expect(page).to have_content brewery_name
+    
+        it "lists the breweries and their total number" do
+            expect(page).to have_content "Number of breweries: #{@breweries.count}"
+            @breweries.each do |brewery_name|
+                expect(page).to have_content brewery_name
+            end
         end
-    end
-
-    it "allows user to navigate to page of a Brewery" do
-        breweries = ["Koff", "Karjala", "Schlenkerla"]
-        year = 1896
-        breweries.each do |brewery_name|
-             FactoryBot.create(:brewery, name: brewery_name, year: year += 1)
+    
+        it "allows user to navigate to page of a Brewery" do
+            click_link "Koff"
+        
+            expect(page).to have_content "Koff"
+            expect(page).to have_content "Established at 1897"
         end
-      
-        visit breweries_path
-      
-        click_link "Koff"
-      
-        expect(page).to have_content "Koff"
-        expect(page).to have_content "Established at 1897"
     end
 end
 
