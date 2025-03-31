67f781c05d565e71aac8999287ae65c1c78a202c
Tamás Tóth <tomjtoth@gmail.com>
2025-03-31 14:11:27 +0300

    adjusted indents in test

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe "Breweries page" do
-  it "should not have any before been created" do
-    visit_n_print breweries_path
-    expect(page).to have_content 'Breweries'
-    expect(page).to have_content 'Number of breweries: 0'
-  end
+    it "should not have any before been created" do
+        visit_n_print breweries_path
+        expect(page).to have_content 'Breweries'
+        expect(page).to have_content 'Number of breweries: 0'
+    end
 end
 
 def visit_n_print(path)
