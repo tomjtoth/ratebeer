5fbfd84844620d858d1532ae613320256b196d5e
Tamás Tóth <tomjtoth@gmail.com>
2025-03-31 14:23:25 +0300

    revert to simple `visit` in tests

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Breweries page" do
     it "should not have any before been created" do
-        visit_n_print breweries_path
+        visit breweries_path
         expect(page).to have_content 'Breweries'
         expect(page).to have_content 'Number of breweries: 0'
     end
