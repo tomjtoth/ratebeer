d7fca34a5187c4545a964c3f9caa9713b99100a7
Tamás Tóth <tomjtoth@gmail.com>
2025-04-01 10:43:36 +0300

    added model BeerClub

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_30_094412) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_01_074256) do
+  create_table "beer_clubs", force: :cascade do |t|
+    t.string "name"
+    t.integer "founded"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "beers", force: :cascade do |t|
     t.string "name"
     t.string "style"
