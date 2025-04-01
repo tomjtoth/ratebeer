43749a35660d5934d767abf24a60a0941265c7f5
Tamás Tóth <tomjtoth@gmail.com>
2025-04-01 10:44:48 +0300

    added model Membership

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_01_074256) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_01_074436) do
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
     t.integer "founded"
@@ -30,6 +30,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_01_074256) do
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "beer_club_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
