205e4b1374680a2f2c6c48792bd7b11050b6d50c
Tamás Tóth <tomjtoth@gmail.com>
2025-04-01 18:36:16 +0300

    added places_spec.rb

---
+++ spec/features/places_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe "Places" do
+  it "shown on page if a single result is returned from API" do
+      allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+        [ Place.new(name: "Oljenkorsi", id: 1) ]
+      )
+
+      kumpula
+
+      expect(page).to have_content "Oljenkorsi"
+    end
+
+  it "shown on page if multiple results is returned from API" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [
+        Place.new(name: "Oljenkorsi1", id: 1),
+        Place.new(name: "Oljenkorsi2", id: 2),
+        Place.new(name: "Oljenkorsi3", id: 3),
+        Place.new(name: "Oljenkorsi4", id: 4),
+     ]
+    )
+
+    kumpula
+
+    expect(page).to have_content "Oljenkorsi1"
+    expect(page).to have_content "Oljenkorsi2"
+    expect(page).to have_content "Oljenkorsi3"
+    expect(page).to have_content "Oljenkorsi4"
+  end
+
+  it "shown on page if multiple results is returned from API" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return([])
+
+    kumpula
+
+    expect(page).to have_content "No locations in kumpula"
+  end
+end
+
+
+def kumpula
+  visit places_path
+  fill_in('city', with: 'kumpula')
+  click_button "Search"
+end
