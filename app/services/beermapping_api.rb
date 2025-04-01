cc77c1adbf6fb92fbb46dd30ccaa9b9e40c692e5
Tamás Tóth <tomjtoth@gmail.com>
2025-04-01 19:57:56 +0300

    adjusted BeermappingApi to handle presumable empty responses

--- app/services/beermapping_api.rb
+++ app/services/beermapping_api.rb
@@ -4,9 +4,14 @@ class BeermappingApi
     url = "http://localhost:55581/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
-    places = response.parsed_response["bmp_locations"]["location"]
 
-    return [] if places.is_a?(Hash) and places['id'].nil?
+    # I have no access to the original API, only guessing here...
+    bmp_locations = response.parsed_response["bmp_locations"]
+    return [] if bmp_locations.nil? || bmp_locations.empty?
+
+    places = bmp_locations["location"]
+
+    return [] if places.nil? || places.is_a?(Hash) and places['id'].nil?
 
     places = [ places ] if places.is_a?(Hash)
     places.map do | place |
