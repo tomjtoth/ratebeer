534a4ce255b6bf002ccd6cc5ec821e9f011280ef
Tamás Tóth <tomjtoth@gmail.com>
2025-04-02 15:37:25 +0300

    redirect_to the beer club's page upon new membership creation and show welcome text

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -28,7 +28,7 @@ class MembershipsController < ApplicationController
 
     respond_to do |format|
       if @membership.save
-        format.html { redirect_to @membership, notice: "Membership was successfully created." }
+        format.html { redirect_to @membership.beer_club, notice: "Welcome to the club, #{current_user.username}!" }
         format.json { render :show, status: :created, location: @membership }
       else
         format.html { render :new, status: :unprocessable_entity }
