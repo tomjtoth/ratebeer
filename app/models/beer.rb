3c3bf6fb366363ba8971845ac201b86457d054ce
Tamás Tóth <tomjtoth@gmail.com>
2025-04-02 19:20:03 +0300

    added one-to-many relation between style and beers

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,6 +4,7 @@ class Beer < ApplicationRecord
   validate :assigned_brewery_exists
 
   belongs_to :brewery
+  belongs_to :style
   has_many :ratings, dependent: :destroy
   has_many :raters, -> { distinct }, through: :ratings, source: :user
   include RatingAverage
