20941a17a4b3ed669c1aaf005df23c8665d158e7
Tamás Tóth <tomjtoth@gmail.com>
2025-04-14 16:42:21 +0300

    tweaked setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# this script preps .env for docker-compose.yml
+
+# rails wants a 32-byte key
+railskeygen () {
+    tr -dc a-f0-9 < /dev/urandom | head -c 32; echo
+}
+
 # if uuidgen does not exist, implement something similar
 if [ -z "$(which uuidgen)" ]; then
   uuidgen () {
@@ -8,7 +15,7 @@ if [ -z "$(which uuidgen)" ]; then
 fi
 
 PG_PASS=$(uuidgen)
-PG_USER=beer-rater
+PG_USER=ratebeer
 PG_DB=ratebeer
 
 echo "\
@@ -17,7 +24,7 @@ echo "\
 BEERMAPPING_APIKEY=_INSERT_YOUR_KEY_HERE_
 WEATHER_APIKEY=_INSERT_YOUR_KEY_HERE_
 
-RAILS_MASTER_KEY=$(uuidgen)
+RAILS_MASTER_KEY=$(railskeygen)
 DATABASE_URL=postgresql://${PG_USER}:${PG_PASS}@db/${PG_DB}
 
 POSTGRES_PASSWORD=${PG_PASS}
